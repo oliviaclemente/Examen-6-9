7899b564a9093c555c8b4704da50ac14442f3524
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-10-18 18:35:53 +0000

    c

---
+++ main.py
@@ -0,0 +1,7 @@
+#ejercicio 6: Realiza una función separar(lista) que toma una lista de números enteros y devuelve dos listas ordenadas. la primera con los números pares y la segunda con los números impares.
+lista= separar([6,5,2,1,7])
+print(pares)
+print(impares)
+[2,6]
+[1,5,7]
+
